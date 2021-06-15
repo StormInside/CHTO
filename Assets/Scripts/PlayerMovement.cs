411eb7de3ebb2cbdc49758c9917d4c2c541c3c5a
StormInside <storminside4766@gmail.com>
2021-06-15 12:53:57 +0300

    player move

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -4,17 +4,30 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-
-    public float speed = 0.3f;
-
-    void Start()
+    public Transform posOne;
+    public Transform posTwo;
+    public float speedX;
+    public Vector2 vc;
+    private void Start()
     {
-        
+        vc = new Vector2(posOne.position.x, posOne.position.y);
     }
 
-    // Update is called once per frame
-    void Update()
+
+    private void FixedUpdate()
     {
-        
+        transform.position = Vector2.MoveTowards(transform.position, vc, Time.deltaTime * speedX);
+        if (transform.position == posOne.position && vc.x != posTwo.position.x)
+        {
+
+            vc.x = posTwo.position.x;
+        }
+        else if (transform.position == posTwo.position && vc.x == posTwo.position.x)
+        {
+            vc.x = posOne.position.x;
+        }
+
     }
+
+
 }
