1e24f86b591f709cdd232fe6f94c8d89c114d9a8
StormInside <storminside4766@gmail.com>
2021-06-15 12:43:38 +0300

    player

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -2,15 +2,19 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class main : MonoBehaviour
+public class PlayerMovement : MonoBehaviour
 {
+
+    public float speed = 0.3f;
 
     void Start()
     {
+        
     }
 
     // Update is called once per frame
     void Update()
     {
+        
     }
 }
